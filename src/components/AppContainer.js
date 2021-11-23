21719046ac84e1e6c244cf7bf92971be9f198f4e
ThiagoDaurizio <thiago_daurizio@hotmail.com>
2021-11-23 17:01:51 -0300

    terça-feira

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,24 +1,145 @@
 import React, { Component } from 'react'
-
+import styled from 'styled-components'
 
 
 
 import CompHeader from "./CompHeader"
-import CompNav from "./CompNav"
-import CompSite from "./CompSite"
-import CompSection from "./CompSection"
+import CompHome from "./CompHome"
 import CompFooter from "./CompFooter"
+import CompForm from './CompForm'
+import CompSection from './CompSection'
+import CompSectionDetails from './CompSectionDetails'
+import CompCart from './CompCart'
 
 export class AppContainer extends Component {
-  State = {
-    inicio: []
+  state = {
+    servicos: [
+      {
+        "title":"Cortar a grama",
+        "description":"Manutenção em áreas verdes de até 1000 metros quadrados.",
+        "price": 40,
+        "paymentMethods": "Pix",
+        "dueDate":"2021-12-30"
+      }, 
+      {
+        "title":"Cortar a arvore",
+        "description":"Manutenção em áreas verdes de até 1000 metros quadrados.",
+        "price": 40,
+        "paymentMethods": "Pix",
+        "dueDate":"2021-12-30"
+      }, 
+    ],
+    carrinho: [],
+    renderizar: 1,
+// -- 1: Home
+// -- 2: Formulário
+// -- 3: Setor de Serviços
+// -- 4: Detalhes do Serviço
+// -- 5: Carrinho
+
+pesquisa: "",
+precoMax: "",
+precoMin: "",
+
+formNome: "",
+formDescricao: "",
+formPreco: "",
+formMetodo: "",
+formData: "",
+
+carrinhoTotal: "",
   }
 
+
+  
+  goToHome = () => {
+    this.setState({renderizar: 1})
+  }
+
+  goToForm = () => {
+    this.setState({renderizar: 2})
+  }
+
+  goToSection = () => {
+    this.setState({renderizar: 3})
+  }
+
+  goToSectionDetails = () => {
+    this.setState({renderizar: 4})
+  }
+
+  goToCart = () => {
+    this.setState({renderizar: 5})
+  }
+
+
+  onChangeFormNome = (event) => {
+      this.setState({formNome: event.target.value})
+      console.log('nome', this.state.formNome)
+  }
+
+  onChangeFormDescricao = (event) => {
+    this.setState({formDescricao: event.target.value})
+    console.log('Descricao', this.state.formDescricao)
+
+  }
+
+  onChangeFormPreco = (event) => {
+    this.setState({formPreco: event.target.value})
+    console.log('Preco', this.state.formPreco)
+  }
+
+  onChangeFormMetodo = (event) => {
+    this.setState({formMetodo: event.target.value})
+    console.log('Metodo', this.state.formMetodo)
+  }
+
+  onChangeFormData = (event) => {
+    this.setState({formData: event.target.value})
+    console.log('Data', this.state.formData)
+  }
+
+
+
+  escolherTela = () => {
+     switch(this.state.renderizar) {
+      case 1: return <CompHome 
+        goToForm={this.goToForm}
+        goToSection={this.goToSection}
+      />
+      case 2: return <CompForm 
+        formNome={this.state.formNome}
+        formDescricao={this.state.formDescricao}
+        formPreco={this.state.formPreco}
+        formMetodo={this.state.formMetodo}
+        formData={this.state.formData}
+
+        onChangeFormNome={this.onChangeFormNome}
+        onChangeFormDescricao={this.onChangeFormDescricao}
+        onChangeFormPreco={this.onChangeFormPreco}
+        onChangeFormMetodo={this.onChangeFormMetodo}
+        onChangeFormData={this.onChangeFormData}
+      />
+      case 3: return <CompSection 
+      servicos={this.props.servicos}  
+      />
+      case 4: return <CompSectionDetails />
+      case 5: return <CompCart />
+      default: return <h1>ERRO</h1>
+     }
+  }
+
+
+
   render() {
+      
     return (
       <div>
-        <CompHeader />
-            <CompSite />
+        <CompHeader 
+        goToHome={this.goToHome}
+        goToCart={this.goToCart}
+        />
+            {this.escolherTela()}
         <CompFooter />
       </div>
     )
--- src/components/CompSection.js
+++ src/components/CompSection.js
@@ -4,21 +4,28 @@ import styled from 'styled-components'
 const StyledSection = styled.div`
 display: flex;
 flex: column;
-background-color: orange
+background-color: orange;
+padding: 10px;
 `
 
 
-export default class CompNav extends Component {
+export default class CompSection extends Component {
   render() {
+      // console.log(this.props.servicos)
+  // const sectionRenderizada = this.props.servicos.map((work) => {
+  //   return (
+  //   <div key={work.id}>
+  //     <p>{work.title}</p>
+  //     <p>{work.description}</p>
+  //     <p>{work.dueDate}</p>
+  //   </div>
+  //         )
+  //   })
+
     return (
       <StyledSection>
-<div>
-    <div>
-        Card do carrinho
-    </div>
-
-    <button type="submit">Contratar</button>
-</div>
+        {/* {sectionRenderizada} */}
+        section
       </StyledSection>
     )
   }
