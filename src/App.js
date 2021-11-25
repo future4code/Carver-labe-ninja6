9415e378f6d65516a9d4a04ff976840c0f8be904
Luis Gustavo <luisgustavordp1@gmail.com>
2021-11-25 14:17:38 -0300

    Cadastro e App.js

--- src/App.js
+++ src/App.js
@@ -6,84 +6,118 @@ import Cards from './Cards';
 import Home from './home';
 import Carrinho from './Carrinho';
 import Detalhes from './Detalhes';
+import axios from 'axios';
 
+const chaveAPI = {
+  headers: {
+    Authorization: '120a13d3-3f0f-4686-84d8-4fb62eee0636',
+  },
+};
 
-class App extends React.Component{
-
+class App extends React.Component {
   state = {
-
-    telaAtual: "Home"
+    telaAtual: 'Home',
+  };
+  componentDidMount() {
+    this.getAllJobs();
   }
 
-  escolherTela = () =>{
-    switch (this.state.telaAtual){
-      
-      case "Home":
-      return <Home 
-      irParaCadastro={this.irParaCadastro}
-      irParaCards={this.irParaCards}
-      irParaCarrinho={this.irParaCarrinho}
-      irParaDetalhes={this.irParaDetalhes}/>
+  getAllJobs = () => {
+    const url = 'https://labeninjas.herokuapp.com/jobs';
+    axios
+      .get({
+        url,
+        chaveAPI,
+      })
+      .then((res) => {
+        console.log('funcionando');
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  };
 
-      case "Cadastro":
-        return<Cadastro 
-      irParaHome={this.irParaHome}
-      irParaCards={this.irParaCards}
-      irParaCarrinho={this.irParaCarrinho}
-      irParaDetalhes={this.irParaDetalhes}/>
+  escolherTela = () => {
+    switch (this.state.telaAtual) {
+      case 'Home':
+        return (
+          <Home
+            irParaCadastro={this.irParaCadastro}
+            irParaCards={this.irParaCards}
+            irParaCarrinho={this.irParaCarrinho}
+            irParaDetalhes={this.irParaDetalhes}
+          />
+        );
 
-      case "Carrinho":
-        return<Carrinho 
-        irParaCadastro={this.irParaCadastro}
-        irParaCards={this.irParaCards}
-        irParaHome={this.irParaHome}
-        irParaDetalhes={this.irParaDetalhes}/>
+      case 'Cadastro':
+        return (
+          <Cadastro
+            irParaHome={this.irParaHome}
+            irParaCards={this.irParaCards}
+            irParaCarrinho={this.irParaCarrinho}
+            irParaDetalhes={this.irParaDetalhes}
+          />
+        );
 
-      case "Detalhes":
-        return<Detalhes 
-      irParaCadastro={this.irParaCadastro}
-      irParaCards={this.irParaCards}
-      irParaCarrinho={this.irParaCarrinho}
-      irParaHome={this.irParaHome}/>
+      case 'Carrinho':
+        return (
+          <Carrinho
+            irParaCadastro={this.irParaCadastro}
+            irParaCards={this.irParaCards}
+            irParaHome={this.irParaHome}
+            irParaDetalhes={this.irParaDetalhes}
+          />
+        );
 
-      case "Cards":
-        return<Cards 
-      irParaCadastro={this.irParaCadastro}
-      irParaHome={this.irParaHome}
-      irParaCarrinho={this.irParaCarrinho}
-      irParaDetalhes={this.irParaDetalhes}/>
-        
+      case 'Detalhes':
+        return (
+          <Detalhes
+            irParaCadastro={this.irParaCadastro}
+            irParaCards={this.irParaCards}
+            irParaCarrinho={this.irParaCarrinho}
+            irParaHome={this.irParaHome}
+          />
+        );
+
+      case 'Cards':
+        return (
+          <Cards
+            irParaCadastro={this.irParaCadastro}
+            irParaHome={this.irParaHome}
+            irParaCarrinho={this.irParaCarrinho}
+            irParaDetalhes={this.irParaDetalhes}
+          />
+        );
+
       default:
-        return <h1>Erro! :(</h1>
+        return <h1>Erro! :(</h1>;
     }
-  }
-  irParaHome = () =>{
-    this.setState({telaAtual: "Home"})
-  }
-  
-  irParaCadastro = () =>{
-    this.setState({telaAtual: "Cadastro"})
-  }
-  irParaCards = () =>{
-    this.setState({telaAtual: "Cards"})
-  }
-  irParaCarrinho = () =>{
-    this.setState({telaAtual: "Carrinho"})
-  }
-  irParaDetalhes = () =>{
-    this.setState({telaAtual: "Detalhes"})
-  }
-  
+  };
+  irParaHome = () => {
+    this.setState({ telaAtual: 'Home' });
+  };
 
-  render(){
-    
+  irParaCadastro = () => {
+    this.setState({ telaAtual: 'Cadastro' });
+  };
+  irParaCards = () => {
+    this.setState({ telaAtual: 'Cards' });
+  };
+  irParaCarrinho = () => {
+    this.setState({ telaAtual: 'Carrinho' });
+  };
+  irParaDetalhes = () => {
+    this.setState({ telaAtual: 'Detalhes' });
+  };
+
+  render() {
     return (
       <div>
-       {this.escolherTela()}
+        {this.escolherTela()}
+        {this.getAllJobs}
       </div>
-    )
+    );
   }
 }
 
-export default App
- 
+export default App;
--- src/Cadastro.js
+++ src/Cadastro.js
@@ -1,5 +1,16 @@
-import React from "react";      
-import { Layout, Menu, Breadcrumb } from 'antd';
+import React, { useState } from 'react';
+import {
+  Layout,
+  Menu,
+  Breadcrumb,
+  Form,
+  Input,
+  Button,
+  Radio,
+  Select,
+  DatePicker,
+  InputNumber,
+} from 'antd';
 import {
   DesktopOutlined,
   PieChartOutlined,
@@ -7,16 +18,73 @@ import {
   TeamOutlined,
   UserOutlined,
 } from '@ant-design/icons';
-import '../src/App.css'
+import '../src/App.css';
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
+const FormSizeDemo = () => {
+  const [componentSize, setComponentSize] = useState('default');
+
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size);
+  };
+
+  return (
+    <>
+      <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="horizontal"
+        initialValues={{
+          size: componentSize,
+        }}
+        onValuesChange={onFormLayoutChange}
+        size={componentSize}
+      >
+        <Form.Item label="Form Size" name="size">
+          <Radio.Group>
+            <Radio.Button value="small">Small</Radio.Button>
+            <Radio.Button value="default">Default</Radio.Button>
+            <Radio.Button value="large">Large</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item label="Título">
+          <Input placeholder="Título" />
+        </Form.Item>
+        <Form.Item label="Descrição">
+          <Input placeholder="Descrição" />
+        </Form.Item>
+        <Form.Item label="Valor">
+          <InputNumber placeholder="Valor" />
+        </Form.Item>
+        <Form.Item label="Formas de Pagamento">
+          <Select mode="multiple">
+            <Select.Option value="Cartão">Cartão</Select.Option>
+            <Select.Option value="dinheiro">Dinheiro</Select.Option>
+            <Select.Option value="PayPal">PayPal</Select.Option>
+            <Select.Option value="Pix">Pix</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label="Prazo">
+          <DatePicker />
+        </Form.Item>
+        <Form.Item label="">
+          <Button>Enviar</Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
 class Cadastro extends React.Component {
   state = {
     collapsed: false,
   };
 
-  onCollapse = collapsed => {
+  onCollapse = (collapsed) => {
     console.log(collapsed);
     this.setState({ collapsed });
   };
@@ -26,19 +94,35 @@ class Cadastro extends React.Component {
     return (
       <Layout style={{ minHeight: '100vh' }}>
         <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-          <img src={"./img/all."} className="logo" />
+          <img src={'./img/all.'} className="logo" alt="" />
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item  onClick={this.props.irParaCards}  key="1" icon={<PieChartOutlined />}>
-             Cards
+            <Menu.Item
+              onClick={this.props.irParaCards}
+              key="1"
+              icon={<PieChartOutlined />}
+            >
+              Cards
             </Menu.Item>
-            <Menu.Item onClick={this.props.irParaHome} key="2" icon={<DesktopOutlined />}>
-             Home
+            <Menu.Item
+              onClick={this.props.irParaHome}
+              key="2"
+              icon={<DesktopOutlined />}
+            >
+              Home
             </Menu.Item>
-          <Menu.Item  onClick={this.props.irParaCarrinho}  key="3" icon={<PieChartOutlined />}>
-             Carrinho
+            <Menu.Item
+              onClick={this.props.irParaCarrinho}
+              key="3"
+              icon={<PieChartOutlined />}
+            >
+              Carrinho
             </Menu.Item>
-            <Menu.Item onClick={this.props.irParaDetalhes} key="4" icon={<DesktopOutlined />}>
-             Detalhes
+            <Menu.Item
+              onClick={this.props.irParaDetalhes}
+              key="4"
+              icon={<DesktopOutlined />}
+            >
+              Detalhes
             </Menu.Item>
             <SubMenu key="sub1" icon={<UserOutlined />} title="User">
               <Menu.Item key="3">Tom</Menu.Item>
@@ -55,21 +139,28 @@ class Cadastro extends React.Component {
           </Menu>
         </Sider>
         <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} >
-        
-          </Header>
+          <Header
+            className="site-layout-background"
+            style={{ padding: 0 }}
+          ></Header>
           <Content style={{ margin: '0 16px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
               <Breadcrumb.Item>User</Breadcrumb.Item>
               <Breadcrumb.Item>Bill</Breadcrumb.Item>
             </Breadcrumb>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              Cadastro
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              <FormSizeDemo />
             </div>
           </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
         </Layout>
       </Layout>
-    )
+    );
   }
-} export default Cadastro
+}
+export default Cadastro;
